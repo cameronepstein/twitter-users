dbe450ea61047d0aef0ea00d0f17c9a28c2e8b91
Cameron Epstein <cameron.a.epstein@gmail.com>
2018-12-13 10:47:16 +0000

    create new twit object

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // Connects to twitter API via twit: https://github.com/ttezel/twit
 const Twit = require('twit');
-var config = require('./config.js');
+const Config = require('./config.js');
 
-console.log(config);
+var T = new Twit({
+  consumer_key: Config.Keys.consumer_key,
+  consumer_secret: Config.Keys.consumer_secret,
+  access_token: Config.Keys.access_token,
+  access_token_secret: Config.Keys.access_token_secret,
+  timeout_ms: 60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL: true,     // optional - requires SSL certificates to be valid.
+})
+
+console.log(T);
